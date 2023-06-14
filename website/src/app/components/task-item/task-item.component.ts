72ad69e94bf89337c2c1fb119c32ca100aff5987
paula hage <hage.paula@hotmail.com>
2023-06-14 12:07:37 +0200

    Created the delete method to delete a single Task

--- website/src/app/components/task-item/task-item.component.ts
+++ website/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,4 +10,10 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent {
   @Input() task: Task = { text: '', day: '', reminder: false };
   faTimes = faTimes;
+
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
 }
